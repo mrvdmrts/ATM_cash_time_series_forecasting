44c7e6bd673b4b52985fb07c38a578ca5ddd5f4b
Merve Demirtas <mrvdmrtss@gmail.com>
2021-01-22 17:34:51 +0300

    Date set as index

--- preprocessing.py
+++ preprocessing.py
@@ -12,13 +12,15 @@ data['month'] = data['Date'].dt.month
 data['year'] = data['Date'].dt.year
 
 # fillna with mean of the month
-#data['CashIn'] = data['CashIn'].fillna(data.groupby(['year', 'month'])['CashIn'].transform('mean'))
-#data['CashOut'] = data['CashOut'].fillna(data.groupby(['year', 'month'])['CashOut'].transform('mean'))
+# data['CashIn'] = data['CashIn'].fillna(data.groupby(['year', 'month'])['CashIn'].transform('mean'))
+# data['CashOut'] = data['CashOut'].fillna(data.groupby(['year', 'month'])['CashOut'].transform('mean'))
 
-#fillna with mean of the week
-data['week_of_month'] = (data['day']- 1) // 7 +1
+# fillna with mean of the week
+data['week_of_month'] = (data['day'] - 1) // 7 + 1
 data['CashIn'] = data['CashIn'].fillna(data.groupby(['month', 'week_of_month'])['CashIn'].transform('mean'))
 data['CashOut'] = data['CashOut'].fillna(data.groupby(['month', 'week_of_month'])['CashOut'].transform('mean'))
 
-#print(data[data['CashOut']<=0])
+# print(data[data['CashOut']<=0])
+# set Date column as index
+data = data.set_index('Date')
 
