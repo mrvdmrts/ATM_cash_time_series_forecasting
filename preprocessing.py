94eb8e1e106f36fc29bebbd8d8727be05356a98a
Merve Demirtas <mrvdmrtss@gmail.com>
2021-01-24 15:31:52 +0300

    fixed code and splitting methods

--- preprocessing.py
+++ preprocessing.py
@@ -2,28 +2,47 @@
 import pandas as pd
 import datetime
 
-# import dataset
-data = pd.read_csv("../Arute_case/ds_exercise_data.csv")
 
-# datetime processing
-data['Date'] = pd.to_datetime(data['Date'], infer_datetime_format=True)
-data['day'] = data['Date'].dt.day
-data['month'] = data['Date'].dt.month
-data['year'] = data['Date'].dt.year
+class Preprocessing:
 
-# fillna with mean of the month
-# data['CashIn'] = data['CashIn'].fillna(data.groupby(['year', 'month'])['CashIn'].transform('mean'))
-# data['CashOut'] = data['CashOut'].fillna(data.groupby(['year', 'month'])['CashOut'].transform('mean'))
+    def __init__(self):
+        # import dataset
+        self.data = pd.read_csv("../Arute_case/ds_exercise_data.csv")
+        return self.data
 
-# fillna with mean of the week
-data['week_of_month'] = (data['day'] - 1) // 7 + 1
-data['CashIn'] = data['CashIn'].fillna(data.groupby(['month', 'week_of_month'])['CashIn'].transform('mean'))
-data['CashOut'] = data['CashOut'].fillna(data.groupby(['month', 'week_of_month'])['CashOut'].transform('mean'))
+    def datetime_partitions(self, df):
+        """
+        Day,month,year partition
+        :param df:ds_exercise_data.csv
+        :return: dataframe with day,month,year columns
+        """
+        df['Date'] = pd.to_datetime(df['Date'], infer_datetime_format=True)
+        df['day'] = df['Date'].dt.day
+        df['month'] = df['Date'].dt.month
+        df['year'] = df['Date'].dt.year
+        return df
 
-# print(data[data['CashOut']<=0])
-# set Date column as index
-data = data.set_index('Date')
+    def fillna_with_mean(self, df):
+        """
+        Filling null with mean of the week
+        :param df: dataframe
+        :return: dataframe
+        """
+        df['week_of_month'] = (df['day'] - 1) // 7 + 1
+        df['CashIn'] = df['CashIn'].fillna(
+            df.groupby(['month', 'week_of_month'])['CashIn'].transform('mean'))
+        df['CashOut'] = df['CashOut'].fillna(
+            df.groupby(['month', 'week_of_month'])['CashOut'].transform('mean'))
+        return df
 
-# train and test split
-train_df = data['2016-01-01':'2019-03-01']
-test_df = data['2019-03-01':'2019-03-31']
+    def train_test_split(self, df):
+        """
+        Splitting train and test
+        :param df: preprocessed dataframe
+        :return: train and test dataframes
+        """
+        df = df.set_index('Date')
+        # train and test split
+        train_df = df['2016-01-01':'2019-03-01']
+        test_df = df['2019-03-01':'2019-03-31']
+        return train_df, test_df
