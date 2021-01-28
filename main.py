0bb49967837663370d105d12e2bb374b75a59f9e
Merve Demirtas <mrvdmrtss@gmail.com>
2021-01-28 21:11:03 +0300

    changed fillna method

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import preprocessing
+import datetime as dt
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib
 
 df = pd.read_csv("../Arute_case/ds_exercise_data.csv")
 df = preprocessing.datetime_partitions(df)
+df = preprocessing.add_holiday(df)
 df = preprocessing.fillna_with_mean(df)
-train, test = preprocessing.train_test_split(df)
-print(test.count())
+
+
+print(df)
+
+# train, test = preprocessing.train_test_split(df)
--- preprocessing.py
+++ preprocessing.py
@@ -5,7 +5,6 @@ import datetime
 
 def datetime_partitions(df):
     """
-    Day,month,year partition
     :param df:ds_exercise_data.csv
     :return: dataframe with day,month,year columns
     """
@@ -13,26 +12,32 @@ def datetime_partitions(df):
     df['day'] = df['Date'].dt.day
     df['month'] = df['Date'].dt.month
     df['year'] = df['Date'].dt.year
+    # It determines the week of a day in a month
+    DAYS_IN_A_WEEK = 7
+    df['week_of_month'] = (df['day'] - 1) // DAYS_IN_A_WEEK + 1
     return df
 
 
 def fillna_with_mean(df):
     """
-    Filling null with mean of the week
-    :param df: dataframe
-    :return: dataframe
+        It fills null values with average cash in and cash out values from same days of the other years
+        :param df: dataframe
+        :return: dataframe
     """
-    df['week_of_month'] = (df['day'] - 1) // 7 + 1
-    df['CashIn'] = df['CashIn'].fillna(
-        df.groupby(['month', 'week_of_month'])['CashIn'].transform('mean'))
-    df['CashOut'] = df['CashOut'].fillna(
-        df.groupby(['month', 'week_of_month'])['CashOut'].transform('mean'))
+    month = df.iloc[(df['CashIn'].isnull()).values, df.columns.get_indexer(['month'])].values.tolist()
+    day = df.iloc[(df['CashIn'].isnull()).values, df.columns.get_indexer(['day'])].values.tolist()
+    mean = []
+    for i in range(0, len(month)):
+        mean.append(
+            df.loc[((df.day == day[i][0]) & (df.month == month[i][0]) & (df.CashIn.notnull())), 'CashIn'].mean())
+
+    df.loc[(df['CashIn'].isnull()), 'CashIn'] = mean
     return df
 
 
 def train_test_split(df):
     """
-    Splitting train and test
+    Splitting train and test data
     :param df: preprocessed dataframe
     :return: train and test dataframes
     """
@@ -40,3 +45,19 @@ def train_test_split(df):
     train_df = df['2016-01-01':'2019-03-01']
     test_df = df['2019-03-01':'2019-03-31']
     return train_df, test_df
+
+
+def add_holiday(df):
+    """
+    :param df: pandas dataframe
+    """
+    df['holiday'] = 0
+    df.loc[((df['month'] == 1) & (df['day'] == 1)) |
+           ((df['month'] == 4) & (df['day'] == 23)) |
+           ((df['month'] == 5) & (df['day'] == 1)) |
+           ((df['month'] == 5) & (df['day'] == 19)) |
+           ((df['month'] == 8) & (df['day'] == 30)) |
+           ((df['month'] == 10) & (df['day'] == 28)) |
+           ((df['month'] == 10) & (df['day'] == 29))
+    , 'holiday'] = 1
+    return  df
